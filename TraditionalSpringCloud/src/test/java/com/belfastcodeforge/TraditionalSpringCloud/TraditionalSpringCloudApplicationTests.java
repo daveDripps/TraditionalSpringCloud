b1e1e8c8474bc9eb36e291c77e04ceb9c32d39fe
David Dripps <dave@thebelfastcodeforge.com>
2019-10-22 00:30:09 +0100

    inital commit

---
+++ TraditionalSpringCloud/src/test/java/com/belfastcodeforge/TraditionalSpringCloud/TraditionalSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belfastcodeforge.TraditionalSpringCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditionalSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
